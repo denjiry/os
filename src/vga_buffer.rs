0d2b221d410f83fdc409b143909c7b8ca373079f
kento tajiri <k.denjiry@gmail.com>
2021-01-18 03:08:20 +0900

    add lazy_static

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -52,6 +52,15 @@ pub struct Writer {
     buffer: &'static mut Buffer,
 }
 
+use lazy_static::lazy_static;
+lazy_static! {
+    pub static ref WRITER: Writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+}
+
 impl Writer {
     pub fn write_byte(&mut self, byte: u8) {
         match byte {
