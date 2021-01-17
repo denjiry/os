b80a3e85dffafba4726b00f15ff4406bb259d610
kento tajiri <k.denjiry@gmail.com>
2021-01-18 04:08:54 +0900

    refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,14 @@
 mod vga_buffer;
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+    loop {}
+}
 
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
