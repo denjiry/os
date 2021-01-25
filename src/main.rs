d18bb280dd6a81b7ce15c91145e70d4719f7d41a
kento tajiri <k.denjiry@gmail.com>
2021-01-26 03:41:42 +0900

    add panic for test

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
 mod serial;
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[no_mangle]
@@ -18,6 +18,7 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
@@ -25,8 +26,17 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
@@ -35,9 +45,9 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... "); // "些末なアサーション……"
-    assert_eq!(1, 1);
-    println!("[ok]");
+    serial_print!("trivial assertion... "); // "些末なアサーション……"
+    assert_eq!(0, 1);
+    serial_println!("[ok]");
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
