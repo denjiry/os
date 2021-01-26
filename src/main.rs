26f0d2a84d6a5f1686ef8c51dda141a594009613
kento tajiri <k.denjiry@gmail.com>
2021-01-27 00:22:04 +0900

    stack overflow for boot-loop

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,19 @@ pub extern "C" fn _start() -> ! {
 
     os::init();
 
-    // trigger a page fault
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
-    };
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+
+    // trigger a stack overflow
+    stack_overflow();
     // // invoke a breakpoint exception
     // x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
