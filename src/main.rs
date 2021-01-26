410df908a1284e5ee571a9e5207798a5d94f346a
kento tajiri <k.denjiry@gmail.com>
2021-01-27 04:41:40 +0900

    change loop to hlt instruction

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,14 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     println!("It did not crash!");
-    loop {
-        use os::print;
-        print!("-");
-    }
+    os::hlt_loop();
 }
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    os::hlt_loop();
 }
 
 #[cfg(test)]
